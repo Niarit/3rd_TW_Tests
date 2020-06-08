a7677b50b1012b40acb66485922feaf5de26bb23
nia <zsofi.deak.nia@gmail.com>
2020-06-08 10:07:27 +0200

    Add multiply browser support

---
+++ 3rd_Tw_tests/src/main/java/main.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class main {
+
+    private WebDriver driver;
+    private final String driverPath = System.getenv("DRIVER_PATH");
+    private final String driverName = System.getenv("DRIVER");
+    private final String browserName = System.getenv("BROWSER");
+
+    @BeforeEach
+    public void setup(){
+        System.setProperty(driverName, driverPath);
+        if (browserName.equals("Firefox")){
+            driver = new FirefoxDriver();
+        } else if(browserName.equals("Chrome")){
+            driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa");
+
+    }
+}
